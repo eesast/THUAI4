97793021f6e9581d5a2065e1c0b1329e7e0c17b6
hesic73 <hesicheng2001@163.com>
2020-10-10 11:10:37 +0800

    Agent CommServer Client 能跑起来，但只进行了单client测试

---
+++ communication/communication_csharp/Agent/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+using Microsoft.Extensions.CommandLineUtils;
+using HPSocket;
+using HPSocket.Tcp;
+using System.Collections.Generic;
+namespace Communication.Agent
+{
+    class Program
+    {
+        private static TcpPackServer server = new TcpPackServer();
+        private static TcpPackClient client = new TcpPackClient();
+        static int Main(string[] args)
+        {
+            var app = new CommandLineApplication();
+            app.HelpOption("-h|--help");
+            var server = app.Option("-s|--server", "game server endpoint.", CommandOptionType.SingleValue);
+            var port = app.Option("-p|--port", "agent port, 7777 in default", CommandOptionType.SingleValue);
+            app.OnExecute(() =>
+            {
+                string ep = server.Value();
+                if (string.IsNullOrEmpty(ep))
+                {
+                    Console.WriteLine("Fatal error: please input server endpoint.");
+                    return 0;
+                }
+                string[] temp = ep.Split(":");
+                if (temp.Length != 2)
+                {
+                    Console.WriteLine("Wrong format of server endpoint. IP:port");
+                    return 0;
+                }
+                string tt = port.Value();
+                return MainInternal(temp[0], ushort.Parse(temp[1]), string.IsNullOrEmpty(tt) ? (ushort)7777 : ushort.Parse(tt));
+            });
+            app.Execute(args);
+            return 0;
+        }
+        private static int MainInternal(string sip, ushort sport, ushort aport)
+        {
+            server.OnReceive += delegate (IServer sender, IntPtr connId, byte[] bytes)
+            {
+                client.Send(bytes, bytes.Length);
+                return HandleResult.Ok;
+            };
+            client.OnReceive += delegate (IClient sender, byte[] bytes)
+            {
+                List<IntPtr> temp = server.GetAllConnectionIds();
+                foreach (IntPtr connId in temp)
+                {
+                    server.Send(connId, bytes, bytes.Length);
+                }
+                return HandleResult.Ok;
+            };
+            client.Address = sip;
+            client.Port = sport;
+            client.Connect();
+            while (!client.IsConnected) Thread.Sleep(100);
+            server.Port = aport;
+            server.Start();
+            Thread.Sleep(int.MaxValue);
+            server.Stop();
+            server.Dispose();
+            client.Stop();
+            client.Dispose();
+            return 0;
+        }
+    }
+}
---
+++ communication/communication_csharp/CommServer/CommServer.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using HPSocket;
+using HPSocket.Tcp;
+namespace Communication.CommServer
+{
+    public delegate void OnReceiveCallback(IntPtr connId);
+    public delegate void OnConnectCallback(IntPtr connId);
+    public sealed class CommServer : IDisposable
+    {
+        private ConcurrentQueue<byte[]> queue;
+        private TcpPackServer server;
+        public event OnReceiveCallback OnReceive;
+        public event OnConnectCallback OnConnect;
+        public CommServer()
+        {
+            server = new TcpPackServer();
+            queue = new ConcurrentQueue<byte[]>();
+            server.OnAccept += delegate (IServer sender, IntPtr connId, IntPtr client)
+            {
+                OnConnect?.Invoke(connId);
+                return HandleResult.Ok;
+            };
+            server.OnReceive += delegate (IServer sender, IntPtr connId, byte[] bytes)
+            {
+                queue.Enqueue(bytes);
+                OnReceive?.Invoke(connId);
+                return HandleResult.Ok;
+            };
+        }
+        public void Listen(ushort port)
+        {
+            server.Port = port;
+            server.Start();
+            Console.WriteLine($"Start listening to port {server.Port}");
+        }
+        public void Send(byte[] bytes)
+        {
+            Console.WriteLine("Boardcast");
+            List<IntPtr> temp = server.GetAllConnectionIds();
+            foreach (IntPtr connId in temp)
+            {
+                server.Send(connId, bytes, bytes.Length);
+            }
+        }
+        public bool Dequeue(out byte[] data)//就是套了个壳，呵呵哒
+        {
+            Console.WriteLine("Dequeueing");
+            return queue.TryDequeue(out data);
+        }
+        public void Stop()
+        {
+            server.Stop();
+        }
+        public void Dispose()
+        {
+            server.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+    }
+    /*
+     class Test
+    {
+        static void Main()
+        {
+            CommServer server = new CommServer();
+            server.Listen(8888);
+            server.OnConnect += delegate (IntPtr connId)
+            {
+                Console.WriteLine($"Client {connId} connects.");
+            };
+            server.OnReceive += delegate (IntPtr connId)
+            {
+                byte[] data;
+                if (server.Dequeue(out data))
+                {
+                    string temp;
+                    temp = System.Text.Encoding.Default.GetString(data);
+                    Console.WriteLine($"Receive from {connId}:" + temp);
+                    char[] arr = temp.ToCharArray();
+                    data = System.Text.Encoding.Default.GetBytes(string.Concat<char>(arr.Reverse<char>()));
+                    server.Send(data);
+                }
+                else
+                {
+                    Console.WriteLine("fail to dequeue");
+                }
+            };
+            Console.ReadLine();
+            server.Dispose();
+            server.Stop();
+        }
+    }
+    */
+}
+
