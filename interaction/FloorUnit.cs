19eba4179f9da9e1967172f6291acd016d2167a9
DreamDraw Pan <panxiurui@outlook.com>
2020-12-04 11:50:02 +0800

    实现了地图染色的接口、一个简单的走路染色的英雄
    调整代码结构，将原有的ColorManager.cs, FloorCreater.cs合并为FloorManager.cs，并在该文件中实现了地图染色、地图创建。
    添加了Hero基类，以及走路染色的英雄WalkHero。

---
+++ interaction/FloorUnit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorUnit : MonoBehaviour
+{
+    //public int colorStatus = 0;
+    public int xPos, yPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void unitColor(Material material)
+    {
+        this.GetComponent<MeshRenderer>().material = material;
+       // Debug.Log(this.GetComponent<MeshRenderer>().material);
+       // this.colorStatus = 1;
+    } 
+}
