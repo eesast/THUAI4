fa9eba35ee40822ba4b63a4b0f7ee1a9f10e7dd6
timothy <liuxf19@163.com>
2021-01-26 20:50:00 +0800

    完成出生点、攻击、地图染色、得分机制

--- logic/LocalUIDll/UI.h
+++ logic/LocalUIDll/UI.h
@@ -17,6 +17,7 @@ private:
 	int width = 0;
 	int height = 0;
 	POINT pixelPerCell;
+	int appendCx = 0;
 	int appendCy = 0;
 
 	HDC hdcMem = NULL;
@@ -24,7 +25,8 @@ private:
 
 	int paintTimerID = 9999;
 
-	long long playerID = 0;
+	long long player1ID = 0;
+	long long player2ID = 0;
 };
 
 #endif
--- logic/THUnity2D/Map.cs
+++ logic/THUnity2D/Map.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using System.Threading;
 using System;
 
@@ -10,37 +11,62 @@ namespace THUnity2D
 		{
 			None = 0,
 			Color1 = 1,
-			Color4 = 4,
 			Color2 = 2,
 			Color3 = 3,
+			Color4 = 4,
+		}
+		public const int maxTeamNum = 4;
+		public ColorType TeamToColor(long teamID)
+		{
+			return (ColorType)(teamID + 1L);
+		}
+		public long ColorToTeam(ColorType color)
+		{
+			return (long)color - 1L;
 		}
 		public struct PlayerInitInfo
 		{
-			public XYPosition initPos;
+			public uint birthPointIdx;
 			public JobType jobType;
 			public long teamID;
-			public PlayerInitInfo(XYPosition initPos, JobType jobType, long teamID)
+			public PlayerInitInfo(uint birthPointIdx, JobType jobType, long teamID)
 			{
-				this.initPos = initPos;
+				this.birthPointIdx = birthPointIdx;
 				this.jobType = jobType;
 				this.teamID = teamID;
 			}
 		}
-		public XYPosition CellToGrid(int x, int y)	//求格子的中心坐标
+		public static XYPosition CellToGrid(int x, int y)	//求格子的中心坐标
 		{
 			XYPosition ret = new XYPosition(x * numOfGridPerCell + numOfGridPerCell / 2, 
 				y * numOfGridPerCell + numOfGridPerCell / 2);
 			return ret;
 		}
-		public int GridToCellX(XYPosition pos)		//求坐标所在的格子的x坐标
+		public static int GridToCellX(XYPosition pos)		//求坐标所在的格子的x坐标
 		{
 			return pos.x / numOfGridPerCell;
 		}
-		public int GridToCellY(XYPosition pos)      //求坐标所在的格子的y坐标
+		public static int GridToCellY(XYPosition pos)      //求坐标所在的格子的y坐标
 		{
 			return pos.y / numOfGridPerCell;
 		}
 		private ColorType[,] cellColor;			//储存每格的颜色
+		public ColorType[,] CellColor
+		{
+			get
+			{
+				int rows = cellColor.GetLength(0), cols = cellColor.GetLength(1);
+				ColorType[,] ret = new ColorType[rows, cols];
+				for (int i = 0; i < rows; ++i)
+				{
+					for (int j = 0; j < cols; ++j)
+					{
+						ret[i, j] = cellColor[i, j];
+					}
+				}
+				return ret;
+			}
+		}
 		public int Rows							//行数
 		{
 			get => cellColor.GetLength(0);
@@ -50,12 +76,10 @@ namespace THUnity2D
 			get => cellColor.GetLength(1);
 		}
 		public const int numOfGridPerCell = 1000;   //每个的坐标单位数
-		public const int numOfStepPerSecond = 50;		//每秒行走的步数
+		public const int numOfStepPerSecond = 50;       //每秒行走的步数
+		public const int addScoreWhenKillOnePlayer = 10;
 
-		public int ObjRadius
-		{
-			get => numOfGridPerCell / 2;
-		}
+		public int ObjRadius { get => numOfGridPerCell / 2; }
 
 		private ArrayList objList;              //游戏对象（除了玩家外）的列表
 		private object objListLock = new object();
@@ -63,18 +87,30 @@ namespace THUnity2D
 		private object playerListLock = new object();
 		private ArrayList teamList;             //队伍列表
 		private object teamListLock = new object();
+		private readonly Dictionary<uint, BirthPoint> birthPointList;	//出生点列表
 
 		private readonly int numOfTeam;
 
 		private readonly int basicPlayerMoveSpeed;
+		private readonly int basicBulletMoveSpeed;
 
 		public long AddPlayer(PlayerInitInfo playerInitInfo)
 		{
-			if (!Team.teamExists(playerInitInfo.teamID)) return GameObject.invalidID;
-			Character newPlayer = new Character(playerInitInfo.initPos, ObjRadius, playerInitInfo.jobType, this.basicPlayerMoveSpeed);
+			if (!Team.teamExists(playerInitInfo.teamID) 
+				|| !MapInfo.ValidBirthPointIdx(playerInitInfo.birthPointIdx) 
+				|| birthPointList[playerInitInfo.birthPointIdx].Parent != null) return GameObject.invalidID;
+
+			XYPosition pos = birthPointList[playerInitInfo.birthPointIdx].Position;
+			Character newPlayer = new Character(pos, ObjRadius, playerInitInfo.jobType, this.basicPlayerMoveSpeed);
+			birthPointList[playerInitInfo.birthPointIdx].Parent = newPlayer;
 			lock (playerListLock) { playerList.Add(newPlayer); }
 			((Team)teamList[(int)playerInitInfo.teamID]).AddPlayer(newPlayer);
 			newPlayer.TeamID = playerInitInfo.teamID;
+
+			//设置出生点的颜色
+
+			int cellX = GridToCellX(pos), cellY = GridToCellY(pos);
+			cellColor[cellX, cellY] = TeamToColor(playerInitInfo.teamID);
 			return newPlayer.ID;
 		}
 
@@ -101,44 +137,6 @@ namespace THUnity2D
 				}
 			}
 			return true;
-		}
-
-		public Map(uint[,] mapResource, int numOfTeam)
-		{
-			//初始化颜色
-			var rows = mapResource.GetLength(0);
-			var cols = mapResource.GetLength(1);
-			cellColor = new ColorType[rows, cols];
-			for (int i = 0; i < rows; ++i)
-				for (int j = 0; j < cols; ++j)
-				{
-					cellColor[i, j] = ColorType.None;
-				}
-
-			this.basicPlayerMoveSpeed = numOfGridPerCell * 2;		//每秒钟行走的坐标数
-
-			//创建线程安全的列表
-			objList = ArrayList.Synchronized(new ArrayList());
-			playerList = ArrayList.Synchronized(new ArrayList());
-			
-			//将墙等游戏对象插入到游戏中
-			for (int i = 0; i < rows; ++i)
-			{
-				for (int j = 0; j < cols; ++j)
-				{
-					if (mapResource[i, j] == (uint)MapInfoObjType.Wall)
-					{
-						lock (objListLock) { objList.Add(new Wall(CellToGrid(i, j), ObjRadius)); }
-					}
-				}
-			}
-
-			this.numOfTeam = numOfTeam;
-			teamList = ArrayList.Synchronized(new ArrayList());
-			for (int i = 0; i < numOfTeam; ++i)
-			{
-				teamList.Add(new Team());
-			}
 		}
 
 		//人物移动
@@ -172,6 +170,14 @@ namespace THUnity2D
 						foreach (GameObject listObj in lst)
 						{
 							if (!listObj.IsRigid || listObj.ID == obj.ID) continue; //不检查自己和非刚体
+
+							if (listObj is BirthPoint)			//如果是出生点，那么除了自己以外的其他玩家需要检查碰撞
+							{
+								//如果是角色并且出生点不是它的出生点，需要检查碰撞，否则不检查碰撞；
+								//下面的条件是obj is Character && !object.ReferenceEquals(((BirthPoint)listObj).Parent, obj)求非得结果
+								if (!(obj is Character) || object.ReferenceEquals(((BirthPoint)listObj).Parent, obj)) continue;
+							}
+
 							int deltaX = nextPos.x - listObj.Position.x, deltaY = nextPos.y - listObj.Position.y;
 							if ((long)deltaX * deltaX + (long)deltaY * deltaY < ((long)obj.Radius + listObj.Radius) * ((long)obj.Radius + listObj.Radius))
 							{
@@ -210,6 +216,13 @@ namespace THUnity2D
 							foreach (GameObject listObj in lst)
 							{
 								if (!listObj.IsRigid || listObj.ID == obj.ID) continue; //不检查自己和非刚体
+
+								if (listObj is BirthPoint)          //如果是出生点，那么除了自己以外的其他玩家需要检查碰撞
+								{
+									//如果出生点是它的出生点，不需要检查碰撞
+									if (object.ReferenceEquals(((BirthPoint)listObj).Parent, obj)) continue;
+								}
+
 								int deltaX = nextPos.x - listObj.Position.x, deltaY = nextPos.y - listObj.Position.y;
 
 								//如果再走一步发生碰撞
@@ -271,7 +284,7 @@ namespace THUnity2D
 				{
 					BulletBomb((Bullet)obj, null); return true;
 				}
-				else if (obj.IsRigid || collisionObj is Character)	//如果碰撞对象可以被炸掉，爆炸
+				else if (obj.IsRigid || collisionObj is Character)	//如果碰撞对象可以被炸掉，爆炸；注意CheckCollision保证了这种情况下collisionObj不可能是BirthPoint
 				{
 					BulletBomb((Bullet)obj, collisionObj); return true;
 				}
@@ -285,6 +298,7 @@ namespace THUnity2D
 			GameObject.Debug(bullet, " bombed!");
 			/*子弹要爆炸时的行为*/
 
+			bullet.CanMove = false;
 			/*从列表中删除*/
 			lock (objListLock)
 			{
@@ -304,10 +318,57 @@ namespace THUnity2D
 				var playerBeingShot = (Character)objBeingShot;
 				if (playerBeingShot.TeamID != bullet.Parent.TeamID)		//如果击中的不是队友
 				{
-					/*未完，受攻击代码*/
+					playerBeingShot.BeAttack(bullet.AP);
+					if (playerBeingShot.HP <= 0)				//如果打死了
+					{
+						//人被打死时会停滞1秒钟
+						playerBeingShot.CanMove = false;
+						lock (playerListLock)
+						{
+							playerList.Remove(playerBeingShot);
+						}
+						playerBeingShot.Reset();
+
+						bullet.Parent.AddScore(addScoreWhenKillOnePlayer);  //给击杀者加分
+
+						Thread.Sleep(1000);
+
+						lock (playerListLock)
+						{
+							playerList.Add(playerBeingShot);
+						}
+						playerBeingShot.CanMove = true;
+					}
 				}
 			}
+
+			/*改变地图颜色*/
+
+			int cellX = GridToCellX(bullet.Position), cellY = GridToCellY(bullet.Position);
+			var colorRange = bullet.GetColorRange();
 			
+			/*哪些颜色不能够被改变*/
+			bool[,] cannotColor = new bool[Rows, Cols];
+			lock (objListLock)
+			{
+				foreach (GameObject obj in objList)
+				{
+					if (obj.IsRigid && (obj is Wall || obj is BirthPoint))
+					{
+						cannotColor[GridToCellX(obj.Position), GridToCellY(obj.Position)] = true;
+					}
+				}
+			}
+
+			foreach (var pos in colorRange)
+			{
+				int colorCellX = cellX + pos.x, colorCellY = cellY + pos.y;
+				if (colorCellX < 0 || colorCellX >= Rows || colorCellY < 0 || colorCellY >= Cols) continue;
+				if (!cannotColor[colorCellX, colorCellY])
+				{
+					cellColor[colorCellX, colorCellY] = TeamToColor(bullet.Parent.TeamID);
+				}
+			}
 		}
 
 		//物体移动
@@ -317,11 +378,9 @@ namespace THUnity2D
 				(
 							() =>
 							{
-								if (!obj.CanMove) return;
-
 								lock (obj.moveLock)
 								{
-									if (obj.IsMoving) return;
+									if (obj.IsMoving || !obj.CanMove) return;
 									obj.IsMoving = true;     //开始移动
 								}
 
@@ -329,9 +388,9 @@ namespace THUnity2D
 								double deltaLen = 0.0;      //储存行走的误差
 								Vector moveVec = new Vector(moveDirection, 0.0);
 								//先转向
-								if (isGaming) deltaLen += moveVec.length - Math.Sqrt(obj.Move(moveVec));     //先转向
+								if (isGaming && obj.CanMove) deltaLen += moveVec.length - Math.Sqrt(obj.Move(moveVec));     //先转向
 								GameObject? collisionObj = null;
-								while (isGaming && moveTime > 0)
+								while (isGaming && moveTime > 0 && obj.CanMove)
 								{
 									var beginTime = Environment.TickCount;
 									moveVec.length = obj.MoveSpeed / numOfStepPerSecond + deltaLen;
@@ -396,7 +455,7 @@ namespace THUnity2D
 						{
 							Bullet newBullet = new Bullet(
 								player.Position + new XYPosition((int)(numOfGridPerCell * Math.Cos(angle)), (int)(numOfGridPerCell * Math.Sin(angle))),
-								ObjRadius, basicPlayerMoveSpeed, player.bulletType);
+								ObjRadius, basicBulletMoveSpeed, player.bulletType, player.AP);
 
 							newBullet.Parent = player;
 							lock (objListLock) { objList.Add(newBullet); }
@@ -417,5 +476,88 @@ namespace THUnity2D
 			lock (objListLock) { gameObjList.AddRange(objList); }
 			return gameObjList;
 		}
+
+		public int GetPlayerScore(long playerID)
+		{
+			foreach (Team team in teamList)
+			{
+				int score;
+				if (team.GetPlayerScore(playerID, out score)) return score;
+			}
+			throw new Exception("GetPlayerScore error: No this player!");
+		}
+		public int GetPlayerHP(long playerID)
+		{
+			foreach (Team team in teamList)
+			{
+				int hp;
+				if (team.GetPlayerHP(playerID, out hp)) return hp;
+			}
+			throw new Exception("GetPlayerHP error: No this player!");
+		}
+		public int GetTeamScore(long teamID)
+		{
+			if (!Team.teamExists(teamID)) throw new Exception("GetTeamScore error: No this team!");
+			int score = ((Team)teamList[(int)teamID]).Score;
+			foreach (var color in cellColor)
+			{
+				if (ColorToTeam(color) == teamID) ++score;
+			}
+			return score;
+		}
+		public Map(uint[,] mapResource, int numOfTeam)
+		{
+			if (numOfTeam > maxTeamNum) throw new Exception("Number of teams overflows!");
+
+			//加入队伍
+			this.numOfTeam = numOfTeam;
+			teamList = ArrayList.Synchronized(new ArrayList());
+			for (int i = 0; i < numOfTeam; ++i)
+			{
+				teamList.Add(new Team());
+			}
+
+			//初始化颜色
+			var rows = mapResource.GetLength(0);
+			var cols = mapResource.GetLength(1);
+			cellColor = new ColorType[rows, cols];
+			for (int i = 0; i < rows; ++i)
+				for (int j = 0; j < cols; ++j)
+				{
+					cellColor[i, j] = ColorType.None;
+				}
+
+			this.basicPlayerMoveSpeed = numOfGridPerCell * 2;       //每秒钟人物行走的坐标数
+			this.basicBulletMoveSpeed = numOfGridPerCell * 5;       //每秒钟子弹行走的坐标数
+
+			//创建线程安全的列表
+			objList = ArrayList.Synchronized(new ArrayList());
+			playerList = ArrayList.Synchronized(new ArrayList());
+			birthPointList = new Dictionary<uint, BirthPoint>(MapInfo.numOfBirthPoint);
+
+			//将墙等游戏对象插入到游戏中
+			for (int i = 0; i < rows; ++i)
+			{
+				for (int j = 0; j < cols; ++j)
+				{
+					switch (mapResource[i, j])
+					{
+					case (uint)MapInfo.MapInfoObjType.Wall:
+						lock (objListLock) { objList.Add(new Wall(CellToGrid(i, j), ObjRadius)); }
+						break;
+					case (uint)MapInfo.MapInfoObjType.BirthPoint1: case (uint)MapInfo.MapInfoObjType.BirthPoint2: case (uint)MapInfo.MapInfoObjType.BirthPoint3: case (uint)MapInfo.MapInfoObjType.BirthPoint4:
+					case (uint)MapInfo.MapInfoObjType.BirthPoint5: case (uint)MapInfo.MapInfoObjType.BirthPoint6: case (uint)MapInfo.MapInfoObjType.BirthPoint7: case (uint)MapInfo.MapInfoObjType.BirthPoint8:
+					{
+						BirthPoint newBirthPoint = new BirthPoint(CellToGrid(i, j), ObjRadius);
+						lock (objListLock) { objList.Add(newBirthPoint); }
+						birthPointList.Add(MapInfo.BirthPointEnumToIdx((MapInfo.MapInfoObjType)mapResource[i, j]), newBirthPoint);
+						break;
+					}
+					}
+				}
+			}
+
+		}
+
 	}
 }
