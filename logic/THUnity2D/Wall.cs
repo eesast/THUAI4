07c4c1103b6b2708b73c19793c78f355f3e06321
timothy <liuxf19@163.com>
2021-02-18 05:03:46 +0800

    基本完成 Server 的功能，需要通信组件的地方已空出，并用 TODO 注释标记

--- logic/THUnity2D/Wall.cs
+++ logic/THUnity2D/Wall.cs
@@ -4,7 +4,7 @@
 
 namespace THUnity2D
 {
-	class Wall : Obj
+	public class Wall : Obj
 	{
 		public Wall(XYPosition initPos, int radius) : base(initPos, radius, true, 0, ObjType.Wall, ShapeType.Sqare) { }
 	}
