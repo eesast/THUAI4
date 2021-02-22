942c86241e3768f185fcd47174320cb7fe191eca
谢童欣 <xietx19@mails.tsinghua.edu.cn>
2021-02-22 13:40:41 +0800

    修复中文乱码

--- logic/Logic.Client/Program.cs
+++ logic/Logic.Client/Program.cs
@@ -17,7 +17,7 @@ namespace Logic.Client
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //ȡ�ÿ�ʼ����
+            //取得开始命令
             AOption? options = null;
             Parser.Default.ParseArguments<AOption>(args).WithParsed(o => { options = o; });
             if (options == null)
@@ -30,45 +30,45 @@ namespace Logic.Client
                 playerID = options.playerID;
                 jobType = (JobType)options.job;
             }
-            //��server����Ϣ
+            //向server发消息
             MessageToServer msg01 = new MessageToServer();
             msg01.MessageType = MessageType.AddPlayer;
-            msg01.TeamID = teamID-1;
-            msg01.PlayerID = playerID-1;
+            msg01.TeamID = teamID - 1;
+            msg01.PlayerID = playerID - 1;
             msg01.JobType = jobType;
-            //TO DO:������Ϣ
+            //TO DO:发出消息
 
-            //�ж��Ƿ�ɹ��������յ���Ϣ��msg02��
+            //判断是否成功：假设收到消息在msg02中
             MessageToOneClient msg02 = new MessageToOneClient();
-            if (!OnReciveShort(msg02)) Application.Exit();//��ʧ�����������
-            else MessageBox.Show("Loading");//�ȴ���Ϸ��ʼ
+            if (!OnReciveShort(msg02)) Application.Exit();//若失败则结束进程
+            else MessageBox.Show("Loading");//等待游戏开始
 
-            //TO DO:һ�յ���Ϣ����OnreciveNormal����
+            //TO DO:一收到消息就用OnreciveNormal处理
 
         }
-        public static int[,] ColorState = new int[50, 50];  //����ÿ����ͼ���Ⱦɫ״̬ 0:δ��Ⱦɫ i:��i��Ⱦɫ -1:ǽ�� -2:������
+        public static int[,] ColorState = new int[50, 50];  //储存每个地图格的染色状态 0:未被染色 i:第i队染色 -1:墙体 -2:出生点
         public static Int64 teamID;
         public static Int64 playerID;
         public static JobType jobType;
-        public static int x; //��ϸ����
-        public static int y; //��ϸ����
+        public static int x; //精细坐标
+        public static int y; //精细坐标
         public static int movespeed;
         private static Dictionary<Int64, Tuple<int, int>> Hashable;
-        private static Form1 gameform; //��Ϸ����
-        private static bool OnReciveShort(MessageToOneClient msg)  //�����Ƿ�ɹ�
+        private static Form1 gameform; //游戏窗体
+        private static bool OnReciveShort(MessageToOneClient msg)  //连接是否成功
         {
             if (msg.MessageType == MessageType.ValidPlayer) return true;
             else return false;
         }
-        private static void OnReciveNormal(MessageToClient msg) //������Ϸ��Ϣ
+        private static void OnReciveNormal(MessageToClient msg) //处理游戏消息
         {
             switch (msg.MessageType)
             {
                 case MessageType.StartGame:
                     Hashable = new Dictionary<Int64, Tuple<int, int>>();
-                    for(int i = 0; i < 4; i++)
+                    for (int i = 0; i < 4; i++)
                     {
-                        for(int j = 0; j < 4; j++)
+                        for (int j = 0; j < 4; j++)
                         {
                             Hashable.Add(msg.PlayerGUIDs[i].TeammateGUIDs[j], new Tuple<int, int>(i, j));
                         }
@@ -84,7 +84,7 @@ namespace Logic.Client
                     gameform.Close();
                     Application.Exit();
                     break;
-                default:break;
+                default: break;
             }
         }
         private static void Refresh(MessageToClient msg)
@@ -92,14 +92,14 @@ namespace Logic.Client
             x = msg.SelfInfo.X;
             y = msg.SelfInfo.Y;
             movespeed = msg.SelfInfo.MoveSpeed;
-            for (int i = 0; i < 50; i++) //��ȡ��ɫ
+            for (int i = 0; i < 50; i++) //读取颜色
             {
                 for (int j = 0; j < 50; j++)
                 {
                     ColorState[i, j] = (int)msg.CellColors[j].RowColors[i];
                 }
             }
-            foreach(var objinfo in msg.GameObjs)
+            foreach (var objinfo in msg.GameObjs)
             {
                 Objdeal(objinfo);
             }
@@ -117,11 +117,11 @@ namespace Logic.Client
                     break;
                 case GameObjType.Character:
                     if (obj.IsDying) break;
-                    Player player = new Player(obj.Y, obj.X, (byte)(obj.TeamID+1), (byte)(Hashable[obj.Guid].Item2+1), obj.JobType, (short)obj.Hp);
+                    Player player = new Player(obj.Y, obj.X, (byte)(obj.TeamID + 1), (byte)(Hashable[obj.Guid].Item2 + 1), obj.JobType, (short)obj.Hp);
                     gameform.DrawPlayer(player);
                     break;
                 case GameObjType.Bullet:
-                    Bullet bullet = new Bullet(obj.Y, obj.X, (int)obj.TeamID+1, obj.BulletType, obj.Guid);
+                    Bullet bullet = new Bullet(obj.Y, obj.X, (int)obj.TeamID + 1, obj.BulletType, obj.Guid);
                     gameform.DrawBullet(bullet);
                     break;
                 case GameObjType.Prop:
@@ -131,10 +131,10 @@ namespace Logic.Client
             }
         }
     }
-    public class Player  //�����ʾ��
+    public class Player  //玩家显示类
     {
-        public int x; //��ϸ����
-        public int y; //��ϸ����
+        public int x; //精细坐标
+        public int y; //精细坐标
         public byte teamnum;
         public byte playernum;
         public JobType job;
@@ -151,15 +151,15 @@ namespace Logic.Client
             this.job = job;
         }
     }
-    public class Bullet  //�ӵ���
+    public class Bullet  //子弹类
     {
-        public int x; //������(��ϸ)
-        public int y; //������(��ϸ)
+        public int x; //横坐标(精细)
+        public int y; //纵坐标(精细)
         public Int64 teamnum;
         public BulletType bulletType;
         public bool existed = false;
         public Int64 id = 0;
-        public Bullet(int x, int y, int teamnum, BulletType bulletType,Int64 guid)
+        public Bullet(int x, int y, int teamnum, BulletType bulletType, Int64 guid)
         {
             this.x = x;
             this.y = y;
@@ -168,22 +168,22 @@ namespace Logic.Client
             this.id = guid;
         }
     }
-    public class Item  //������
+    public class Item  //道具类
     {
-        public int xnum; //��������
-        public int ynum; //��������
+        public int xnum; //横坐标大格
+        public int ynum; //纵坐标大格
         public byte type = 0;
         /*
          * 0:NULL
-         * 1:����
-         * 2:����
-         * 4:��Ѫ
-         * 5:��
-         * 6:�����
-         * 7:�ƶ�
-         * 8:���ٵ���
-         * 9:���˵���
-         * 10:��CD����
+         * 1:加速
+         * 2:加伤
+         * 4:回血
+         * 5:盾
+         * 6:复活甲
+         * 7:破盾
+         * 8:减速地雷
+         * 9:减伤地雷
+         * 10:加CD地雷
          */
         public bool existed = false;
         public long id = 0;
@@ -192,8 +192,7 @@ namespace Logic.Client
             this.xnum = xnum;
             this.ynum = ynum;
             this.type = (byte)type;
-            this.id = xnum*50+ynum;
+            this.id = xnum * 50 + ynum;
         }
     }
-
 }
