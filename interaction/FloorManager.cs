fcaef56fcd30f1e0702dea3ba2247369c650b403
DreamDraw Pan <panxiurui@outlook.com>
2021-02-25 23:28:11 +0800

    Update FloorManager.cs

--- interaction/FloorManager.cs
+++ interaction/FloorManager.cs
@@ -1,36 +1,29 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
+using Es.InkPainter;
 
 public class FloorManager : MonoBehaviour{
     public const int MAP_SIZE = 19;
-    public GameObject funit;
-    public Object[,] FloorUnit;
     public static string[,] FloorColor;
     public static bool[,] ColorChanged;
     public static Material[] Materials;
     public static Dictionary<string, int> Color2Index;
     
     void Start() {
-        FloorUnit = new Object[MAP_SIZE,MAP_SIZE];
         FloorColor = new string[MAP_SIZE,MAP_SIZE];
-        ColorChanged = new bool[MAP_SIZE, MAP_SIZE];
+        //ColorChanged = new bool[MAP_SIZE, MAP_SIZE];
         Color2Index = new Dictionary<string, int>();
         
         CreateTerrain();
         
-        Materials = Resources.LoadAll<Material>("Materials");
-        Color2Index.Add("brown", 0);
-        Color2Index.Add("green", 1);
-        Color2Index.Add("grey", 2);
-        Color2Index.Add("yellow", 3);
-        Color2Index.Add("blue", 4);
-
-        for(int i = 0; i < MAP_SIZE; i++) 
-            for(int j = 0; j < MAP_SIZE; j++) 
-                SetColor(i, j, "yellow");
+        Vector3 pos1 = new Vector3(3, 0, 3);
+        Vector3 pos2 = new Vector3(4, 0, 4);
+        GetComponent<SomewherePainter>().ScopePaint(pos1, pos2);
+        /*Materials = Resources.LoadAll<Material>("Materials");
+        */
     }
 
     // Update is called once per frame
@@ -38,39 +31,15 @@ public class FloorManager : MonoBehaviour{
     void Update() {
         //处理信息队列
         
-        UpdateColor();
+        //UpdateColor();
     }
     
-    void UpdateColor(){
-        for(int i = 0; i < MAP_SIZE; i++) {
-            for(int j = 0; j < MAP_SIZE; j++) {
-                if (!ColorChanged[i, j])
-                    continue;
-                GameObject tmp = FloorUnit[i, j] as GameObject;
-                tmp.GetComponent<FloorUnit>().unitColor(Materials[Color2Index[FloorColor[i, j]]]);
-                ColorChanged[i, j] = false;
-            }
-        }
-    }
-
-    public static void SetColor(int xPos, int yPos, string unitColor){
-        FloorColor[xPos, yPos] = unitColor;
-        ColorChanged[xPos, yPos] = true;
-    }
     //Onrecieve()
 
     void CreateTerrain(){
         //这个函数用来创建地形，包括地面、墙壁等
         
         //地面
-        for(int i = 0; i < MAP_SIZE; i++) {
-             for(int j = 0; j < MAP_SIZE; j++) {
-                 FloorUnit[i, j] = Instantiate(funit, new Vector3((i-MAP_SIZE/2)*1.0f, 0f, (j-MAP_SIZE/2)*1.0f), Quaternion.identity);
-                 GameObject tmp = FloorUnit[i, j] as GameObject;
-                 tmp.GetComponent<FloorUnit>().xPos = i;
-                 tmp.GetComponent<FloorUnit>().yPos = j;
-             }
-        }
         
         //墙壁
         
