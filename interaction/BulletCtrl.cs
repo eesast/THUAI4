8bfd54f6c767bdcbc078ed54df8359a0d895c7b1
Xiurui Pan <panxiurui@outlook.com>
2021-02-28 10:19:08 +0800

    实现了子弹染色

---
+++ interaction/BulletCtrl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCtrl : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public String destName = "Plane";
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speed*Time.deltaTime, 0, 0);
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        Debug.Log(other.gameObject.name);
+        
+        if(other.gameObject.name == destName)
+            Destroy(gameObject);
+    }
+}
