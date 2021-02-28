3054cf7fc7d316d8a6bd77adf1b38d558dbe9fbf
timothy <liuxf19@163.com>
2021-02-28 22:34:40 +0800

    pull eesast

---
+++ interaction/FireCtrl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCtrl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public int lineLength = 1000;
+    public GameObject obj;
+
+    
+    
+    void Start()
+    {
+        
+    }
+
+// Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Instantiate(obj, transform.position, transform.rotation); //.GetComponent<Rigidbody>().velocity = speed * transform.forward;
+        }
+    }
+}
