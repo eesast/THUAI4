a1214da3ced06c987417e1c0daceb4d43f4b3927
hesic73 <hesicheng2001@163.com>
2021-03-04 14:18:01 +0800

    改为936编码

--- CAPI/CAPI/CAPI/Constants.h
+++ CAPI/CAPI/CAPI/Constants.h
@@ -17,7 +17,7 @@ namespace Constants
 	static const int32_t MessageToClient = 0;
 	static const int32_t MessageToServer = 1;
 	static const int32_t MessageToOneClient = 2;
-	//**数值是我随便写的**
+
 	static const uint64_t SightRadius = 5000;
 	static const uint64_t SightRadiusSquared = SightRadius * SightRadius;
 	static const uint32_t numOfGridPerCell = 1000;
