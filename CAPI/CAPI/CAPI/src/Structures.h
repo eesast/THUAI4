98c9ae99431a28ea82a7b1c1434da064fb2f65cf
hesic73 <hesicheng2001@163.com>
2021-03-15 14:36:10 +0800

    把GUID由uint64改为int64与proto相符，初步写了下CAPI介绍

--- CAPI/CAPI/CAPI/src/Structures.h
+++ CAPI/CAPI/CAPI/src/Structures.h
@@ -10,7 +10,7 @@
 namespace THUAI4
 {
 
-	enum class PropType : unsigned char
+	enum class PropType : unsigned char    //道具类型
 	{
 		Null = 0,
 		Bike = 1,
@@ -25,13 +25,13 @@ namespace THUAI4
 		Divider = 10
 	};
 
-	enum class ShapeType : unsigned char
+	enum class ShapeType : unsigned char    //物体形状
 	{
 		Circle = 0,
 		Square = 1
 	};
 
-	enum class JobType : unsigned char
+	enum class JobType : unsigned char    //玩家职业
 	{
 		Job0 = 0,
 		Job1 = 1,
@@ -42,7 +42,7 @@ namespace THUAI4
 		Job6 = 6,
 	};
 
-	enum class BulletType : unsigned char
+	enum class BulletType : unsigned char    //子弹种类
 	{
 		Bullet0 = 0,
 		Bullet1 = 1,
@@ -53,7 +53,7 @@ namespace THUAI4
 		Bullet6 = 6
 	};
 
-	enum class ColorType : unsigned char
+	enum class ColorType : unsigned char    //颜色种类
 	{
 		None = 0,
 		Color1 = 1,
@@ -65,75 +65,75 @@ namespace THUAI4
 
 	struct Character
 	{
-		bool isMoving;
-		bool isDying;
-		ShapeType shapeType : 2;
-		BulletType bulletType : 4;
-		PropType propType : 4;
-		JobType jobType : 4;
-		uint16_t lifeNum;
-		uint16_t teamID;
-		uint16_t radius;
-		uint16_t maxBulletNum;
-		uint16_t bulletNum;
+		bool isMoving;  //是否正在移动
+		bool isDying;  //是否正在复活中
+		ShapeType shapeType : 2;  //形状（圆）
+		BulletType bulletType : 4;  //子弹种类
+		PropType propType : 4;  //所拥有道具类型
+		JobType jobType : 4;  //职业类型
+		uint16_t lifeNum;  //第几条命
+		uint16_t teamID;  //队伍ID
+		uint16_t radius;  //圆形物体的半径或正方形内切圆半径
+		uint16_t maxBulletNum;  //弹夹最大容量
+		uint16_t bulletNum;  //当前子弹数量
 
-		uint32_t x;
-		uint32_t y;
-		uint32_t moveSpeed;
-		uint32_t ap;
-		uint32_t maxHp;
-		uint32_t hp;
-		uint32_t CD;
-		uint64_t guid;
-		double facingDirection;
+		uint32_t x;  //x坐标
+		uint32_t y;  //y坐标
+		uint32_t moveSpeed;  //移动速度
+		uint32_t ap;  //攻击力
+		uint32_t maxHp;  //最大生命值
+		uint32_t hp;  //当前声明值
+		uint32_t CD;  //设计冷却时间
+		int64_t guid;  //Global Unique Identifier
+		double facingDirection;  //移动朝向
 	};
 
 	struct Wall
 	{
-		ShapeType shapeType : 2;
-		uint16_t radius;
-		uint32_t x;
-		uint32_t y;
-		uint64_t guid;
+		ShapeType shapeType : 2;  //形状（正方形）
+		uint16_t radius;  //圆形物体的半径或正方形内切圆半径
+		uint32_t x;  //x坐标
+		uint32_t y;  //y坐标
+		int64_t guid;  //Global Unique Identifier
 	};
 
 	struct Prop
 	{
-		bool isMoving;
-		bool isLaid;
-		ShapeType shapeType : 2;
-		PropType propType : 4;
-		uint16_t radius;
-		uint32_t x;
-		uint32_t y;
-		uint32_t moveSpeed;
-		uint64_t guid;
-		double facingDirection;
+		bool isMoving;  //是否正在移动
+		bool isLaid;  //是否已被放置（选手能看到的一定没被放置）
+		ShapeType shapeType : 2;  //形状
+		PropType propType : 4;  //道具种类
+		uint16_t radius;  //圆形物体的半径或正方形内切圆半径
+		uint32_t x;  //x坐标
+		uint32_t y;  //y坐标
+		uint32_t moveSpeed;  //移动速度
+		int64_t guid;  //Global Unique Identifier
+		double facingDirection;  //移动朝向
 	};
 
 	struct Bullet
 	{
-		bool isMoving;
-		ShapeType shapeType : 2;
-		BulletType bulletType : 4;
-		uint16_t radius;
-		uint16_t teamID;
-		uint32_t x;
-		uint32_t y;
-		uint32_t moveSpeed;
-		uint32_t ap;
-		uint64_t guid;
-		double facingDirection;
+		bool isMoving;  //是否在移动（是）
+		ShapeType shapeType : 2;  //形状（圆）
+		BulletType bulletType : 4;  //子弹种类
+		uint16_t radius;  //圆形物体的半径或正方形内切圆半径
+		uint16_t teamID;  //所属队伍
+		uint32_t x;  //x坐标
+		uint32_t y;  //y坐标
+		uint32_t moveSpeed;  //移动速度
+		uint32_t ap;  //攻击力
+		int64_t guid;  //Global Unique Identifier
+		double facingDirection;  //移动朝向
 	};
 
 	struct BirthPoint
 	{
-		ShapeType shapeType;
-		uint16_t teamID;
-		uint16_t radius;
-		uint32_t x;
-		uint32_t y;
-		uint64_t guid;
+		ShapeType shapeType;  //形状（圆）
+		uint16_t teamID;  //队伍ID
+		uint16_t radius;  //圆形物体的半径或正方形内切圆半径
+		uint32_t x;  //x坐标
+		uint32_t y;  //y坐标
+		int64_t guid;  //Global Unique Identifier
 	};
 
 }
