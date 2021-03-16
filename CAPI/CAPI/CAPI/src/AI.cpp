53c0abcd1086e034eda8cd5cbaed97211780314a
timothy <liuxf19@163.com>
2021-03-16 15:44:58 +0800

    Players should now choose their job in AI.cpp by modifying the variable playerJob --- instead of passing it through the command argument.

--- CAPI/CAPI/CAPI/src/AI.cpp
+++ CAPI/CAPI/CAPI/src/AI.cpp
@@ -5,8 +5,13 @@
 
 /* 请于 VS2019 项目属性中开启 C++17 标准：/std:c++17 */
 
-std::uniform_real_distribution<double> direction(0, 2 * 3.1415926);
-std::default_random_engine e;
+extern const THUAI4::JobType playerJob = THUAI4::JobType::Job0;	//选手职业，选手 !!必须!! 定义此变量来选择职业
+
+namespace
+{
+	std::uniform_real_distribution<double> direction(0, 2 * 3.1415926);
+	std::default_random_engine e;
+}
 
 void AI::play(GameApi& g)
 {
--- CAPI/CAPI/CAPI/src/AI.h
+++ CAPI/CAPI/CAPI/src/AI.h
@@ -6,6 +6,8 @@
 
 #include "Base.h"
 
+
+
 class AI : public AIBase
 {
 public:
