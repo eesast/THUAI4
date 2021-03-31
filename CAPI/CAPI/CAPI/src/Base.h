c26e468ec80907678168270376dfd90420367867
timothy <liuxf19@163.com>
2021-04-01 01:27:59 +0800

    Modify: 修改了部分角色属性，增加了 Constant.h 对角色属性的说明，并对接口有微笑修改

--- CAPI/CAPI/CAPI/src/Base.h
+++ CAPI/CAPI/CAPI/src/Base.h
@@ -33,21 +33,22 @@ public:
 	virtual void Send(int toPlayerID, std::string message) = 0;
 
 	//选手可获取的信息
-	virtual bool MessageAvailable() = 0;
-	virtual bool TryGetMessage(std::string &) = 0;
+	[[nodiscard]] virtual bool MessageAvailable() = 0;
+	[[nodiscard]] virtual bool TryGetMessage(std::string&) = 0;
 
-	virtual std::vector<std::shared_ptr<const THUAI4::Character>> GetCharacters() const = 0;
-	virtual std::vector<std::shared_ptr<const THUAI4::Wall>> GetWalls() const = 0;
-	virtual std::vector<std::shared_ptr<const THUAI4::Prop>> GetProps() const = 0;
-	virtual std::vector<std::shared_ptr<const THUAI4::Bullet>> GetBullets() const = 0;
-	virtual std::vector<std::shared_ptr<const THUAI4::BirthPoint>> GetBirthPoints() const = 0;
-	virtual std::shared_ptr<const THUAI4::Character> GetSelfInfo() const = 0;
+	[[nodiscard]] virtual std::vector<std::shared_ptr<const THUAI4::Character>> GetCharacters() const = 0;
+	[[nodiscard]] virtual std::vector<std::shared_ptr<const THUAI4::Wall>> GetWalls() const = 0;
+	[[nodiscard]] virtual std::vector<std::shared_ptr<const THUAI4::Prop>> GetProps() const = 0;
+	[[nodiscard]] virtual std::vector<std::shared_ptr<const THUAI4::Bullet>> GetBullets() const = 0;
+	[[nodiscard]] virtual std::vector<std::shared_ptr<const THUAI4::BirthPoint>> GetBirthPoints() const = 0;
+	[[nodiscard]] virtual std::shared_ptr<const THUAI4::Character> GetSelfInfo() const = 0;
 
-	virtual THUAI4::ColorType GetSelfTeamColor() const = 0;
-	virtual uint32_t GetTeamScore() const = 0;
+	[[nodiscard]] virtual THUAI4::ColorType GetSelfTeamColor() const = 0;
+	[[nodiscard]] virtual uint32_t GetTeamScore() const = 0;
+
 	//GetPlayerGUIDS() 底层是 array 而且一局游戏内不变 so就这样
-	virtual const std::array<std::array<int64_t, StateConstant::nPlayers>, StateConstant::nTeams> &GetPlayerGUIDs() const = 0;
-	virtual THUAI4::ColorType GetCellColor(int CellX, int CellY) const = 0;
+	[[nodiscard]] virtual const std::array<std::array<int64_t, StateConstant::nPlayers>, StateConstant::nTeams>& GetPlayerGUIDs() const = 0;
+	[[nodiscard]] virtual THUAI4::ColorType GetCellColor(int CellX, int CellY) const = 0;
 };
 
 class AIBase
@@ -66,8 +67,8 @@ private:
 	static inline int teamID = 0;
 
 public:
-	static int GetPlayerID() { return playerID; }
-	static int GetTeamID() { return teamID; }
+	[[nodiscard]] static int GetPlayerID() { return playerID; }
+	[[nodiscard]] static int GetTeamID() { return teamID; }
 
 	friend int thuai4_main(int argc, char **argv, CreateAIFunc AIBuilder);
 };
