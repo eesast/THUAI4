23f9c9ff924e30bab766bdb7006bfc108fc0d935
timothy <liuxf19@163.com>
2021-04-13 21:27:42 +0800

    Add: a project for unity to read playback file

--- playback/playback/MessageWriter.cs
+++ playback/playback/MessageWriter.cs
@@ -50,23 +50,4 @@ namespace playback
 			Dispose();
 		}
 	}
-	//public static class PlayBackProducer
-	//{
-	//	public static void WriteMessage(string fileName, MessageToClient[] msgs)
-	//	{
-	//		using (FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
-	//		{
-	//			using (CodedOutputStream cos = new CodedOutputStream(fs))
-	//			{
-	//				cos.WriteFixed64((ulong)msgs.Length);
-	//				foreach (var msg in msgs)
-	//				{
-	//					cos.WriteMessage(msg);
-	//				}
-	//			}
-	//		}
-	//	}
-
-	//	public static void 
-	//}
 }
