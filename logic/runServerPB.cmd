1752fc3c6da6a5418e2be9b8d4e2c0b88fa3b93f
timothy <liuxf19@163.com>
2021-05-12 23:48:03 +0800

    Fix bugs: check the legalness of the argument of message from client

---
+++ logic/runServerPB.cmd
@@ -0,0 +1,3 @@
+@echo off
+.\Logic.Server\bin\Release\netcoreapp3.1\Logic.Server.exe --port=20000 --teamCount=2 --playerCount=4 -g 600 --fileName=private\res\5z --playBack --playBackSpeed=4.0
+pause
