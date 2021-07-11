285032f2e1531f1f17c90361d0e546b1cd77f5a3
timothy <liuxf19@163.com>
2021-07-11 16:23:29 +0800

    Move cmds into a folder

--- logic/runServer.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-.\Logic.Server\bin\Release\netcoreapp3.1\Logic.Server.exe --port=20000 --teamCount=2 --playerCount=4 -g 600
-pause
--- logic/runServerDebugPB.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-.\Logic.Server\bin\Debug\netcoreapp3.1\Logic.Server.exe --port=20000 --playBack --playBackSpeed=1.0 --fileName=video
